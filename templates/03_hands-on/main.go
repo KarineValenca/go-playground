4a6ea0c1f3aacff29d5ca7e2d3b6d097b9fbdb1f
Karine Valenca <valenca.karine@gmail.com>
2020-04-07 21:01:53 -0300

    added last exercise of templating

--- templates/03_hands-on/main.go
+++ templates/03_hands-on/main.go
@@ -16,6 +16,11 @@ type meal struct {
 	Items []item
 }
 
+type restaurant struct {
+	Name string
+	Menu []meal
+}
+
 
 var tpl *template.Template
 
@@ -23,55 +28,61 @@ func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 func main () {
-	menu := []meal{
-		meal{
-			Meal: "Breakfast",
-			Items: []item{
-				item{
-					Name: "Pancake",
-					Description: "Japanese or American Pancakes",
-					Price: 1.95,
+	restaurants := []restaurant{
+		restaurant{
+			Name: "Amazing food",
+			Menu: []meal{
+				meal{
+					Meal: "Breakfast",
+					Items: []item{
+						item{
+							Name: "Pancake",
+							Description: "Japanese or American Pancakes",
+							Price: 1.95,
+						},
+						item{
+							Name: "Fruits",
+							Description: "Fresh Fruits",
+							Price: 0.95,
+						},
+					},
 				},
-				item{
-					Name: "Fruits",
-					Description: "Fresh Fruits",
-					Price: 0.95,
+				meal{
+					Meal: "Lunch",
+					Items: []item{
+						item{
+							Name: "Lasagna",
+							Description: "Italian Lasagna",
+							Price: 9.99,
+						},
+						item{
+							Name: "Feijoada",
+							Description: "Brazilian Feijoada",
+							Price: 20.55,
+						},
+					},
 				},
-			},
-		},
-		meal{
-			Meal: "Lunch",
-			Items: []item{
-				item{
-					Name: "Lasagna",
-					Description: "Italian Lasagna",
-					Price: 9.99,
-				},
-				item{
-					Name: "Feijoada",
-					Description: "Brazilian Feijoada",
-					Price: 20.55,
-				},
-			},
-		},
-		meal{
-			Meal: "Dinner",
-			Items: []item{
-				item{
-					Name: "Pizza",
-					Description: "Italian Pizza",
-					Price: 9.99,
-				},
-				item{
-					Name: "Sushi",
-					Description: "Japanese Sushi",
-					Price: 20.55,
+				meal{
+					Meal: "Dinner",
+					Items: []item{
+						item{
+							Name: "Pizza",
+							Description: "Italian Pizza",
+							Price: 9.99,
+						},
+						item{
+							Name: "Sushi",
+							Description: "Japanese Sushi",
+							Price: 20.55,
+						},
+					},
 				},
 			},
 		},
 	}
+	
 
-	err := tpl.Execute(os.Stdout, menu)
+	err := tpl.Execute(os.Stdout, restaurants)
 	if err != nil {
 		log.Fatalln(err)
 	}
