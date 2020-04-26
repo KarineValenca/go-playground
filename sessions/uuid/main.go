263124cb99b94c1eeb11cbdd66bf4ca1ec4d503e
Karine Valenca <valenca.karine@gmail.com>
2020-04-26 19:24:39 -0300

    adding exercises about sessions

---
+++ sessions/uuid/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/satori/go.uuid"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("session")
+	if err != nil {
+		id, _ := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name: "session",
+			Value: id.String(),
+			HttpOnly: true,
+			Path: "/",
+		}
+		http.SetCookie(w, cookie)
+	}
+	fmt.Println(cookie)
+}
