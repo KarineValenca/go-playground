c8457a8be9da85a8e0c4fa91c86adc1a41fc1611
Karine Valenca <valenca.karine@gmail.com>
2020-04-27 20:57:56 -0300

    added logout, permissions and expire sessions

--- sessions/session/main.go
+++ sessions/session/main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"net/http"
 	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
 type user struct {
@@ -12,14 +13,23 @@ type user struct {
 	Password []byte
 	First string
 	Last string
+	Role string
+}
+
+type session struct {
+	un string
+	lastActivity time.Time
 }
 
 var tpl *template.Template
 var dbUsers = map[string]user{}
-var dbSessions = map[string]string{}
+var dbSessions = map[string]session{}
+var dbSessionsCleaned time.Time
+const sessionLength int = 300
 
 func init(){
 	tpl = template.Must(template.ParseGlob("templates/*"))
+	dbSessionsCleaned = time.Now()
 }
 
 func main() {
@@ -27,26 +37,31 @@ func main() {
 	http.HandleFunc("/bar", bar)
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	u := getUser(req)
+	u := getUser(w, req)
 	tpl.ExecuteTemplate(w, "index.gohtml", u)
 }
 
 func bar(w http.ResponseWriter, req *http.Request) {
-	u := getUser(req)
-	if !alreadyLoggedIn(req) {
+	u := getUser(w, req)
+	if !alreadyLoggedIn(w, req) {
 		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	if u.Role != "007" {
+		http.Error(w, "You must be 007 to enter the bar", http.StatusForbidden)
 		return
 	}
 	tpl.ExecuteTemplate(w, "bar.gohtml", u)
 }
 
 func signup(w http.ResponseWriter, req *http.Request) {
-	if alreadyLoggedIn(req) {
+	if alreadyLoggedIn(w, req) {
 		http.Redirect(w, req, "/", http.StatusSeeOther)
 		return
 	}
@@ -57,6 +72,7 @@ func signup(w http.ResponseWriter, req *http.Request) {
 		p := req.FormValue("password")
 		f := req.FormValue("firstname")
 		l := req.FormValue("lastname")
+		r := req.FormValue("role")
 
 		// checks if username is already taken
 		if _, ok := dbUsers[un]; ok {
@@ -71,13 +87,13 @@ func signup(w http.ResponseWriter, req *http.Request) {
 			Value: sID.String(),
 		}
 		http.SetCookie(w, c)
-		dbSessions[c.Value] = un
+		dbSessions[c.Value] = session{un, time.Now()}
 
 		bs, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
 		if err != nil {
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 		}
-		u := user{un, bs, f, l}
+		u := user{un, bs, f, l, r}
 		dbUsers[un] = u
 		http.Redirect(w, req, "/", http.StatusSeeOther)
 		return
@@ -87,7 +103,7 @@ func signup(w http.ResponseWriter, req *http.Request) {
 }
 
 func login(w http.ResponseWriter, req *http.Request) {
-	if alreadyLoggedIn(req) {
+	if alreadyLoggedIn(w, req) {
 		http.Redirect(w, req, "/", http.StatusSeeOther)
 		return
 	}
@@ -114,7 +130,7 @@ func login(w http.ResponseWriter, req *http.Request) {
 			Value: sID.String(),
 		}
 		http.SetCookie(w, c)
-		dbSessions[c.Value] = un
+		dbSessions[c.Value] = session{un, time.Now()}
 		http.Redirect(w, req, "/", http.StatusSeeOther)
 		return
 	}
@@ -122,4 +138,27 @@ func login(w http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(w, "login.gohtml", nil)
 }
 
+func logout(w http.ResponseWriter, req *http.Request) {
+	if !alreadyLoggedIn(w, req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	c, _ := req.Cookie("session")
+	delete(dbSessions, c.Value)
+	c = &http.Cookie{
+		Name: "session",
+		Value: "",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, c)
+	
+
+	// clean up dbSessions
+	if time.Now().Sub(dbSessionsCleaned) > (time.Second * 30) {
+		go cleanSessions()
+	}
+
+	http.Redirect(w, req, "/login", http.StatusSeeOther)
+}
 
--- sessions/session/sessions.go
+++ sessions/session/sessions.go
@@ -2,29 +2,68 @@ package main
 
 import (
 	"net/http"
+	"github.com/satori/go.uuid"
+	"fmt"
+	"time"
 )
 
-func getUser(req *http.Request) user {
+func getUser(w http.ResponseWriter, req *http.Request) user {
 	var u user
 
 	c, err := req.Cookie("session")
 	if err != nil {
-		return u
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
+			Name: "session",
+			Value: sID.String(),
+		}
 	}
+	c.MaxAge = sessionLength
+	http.SetCookie(w, c)
 
-	if un, ok := dbSessions[c.Value]; ok {
-		u = dbUsers[un]
+	if s, ok := dbSessions[c.Value]; ok {
+		s.lastActivity = time.Now()
+		dbSessions[c.Value] = s
+		u = dbUsers[s.un]
 	}
 
 	return u
 }
 
-func alreadyLoggedIn(req *http.Request) bool {
+func alreadyLoggedIn(w http.ResponseWriter, req *http.Request) bool {
 	c, err := req.Cookie("session")
 	if err != nil {
 		return false
 	}
-	un := dbSessions[c.Value]
-	_, ok := dbUsers[un]
+	s, ok := dbSessions[c.Value]
+	if ok {
+		s.lastActivity = time.Now()
+		dbSessions[c.Value] = s
+	}
+
+	_, ok = dbUsers[s.un]
+	c.MaxAge = sessionLength
+	http.SetCookie(w, c)
 	return ok
+}
+
+func cleanSessions() {
+	fmt.Println("BEFORE CLEAN")
+	showSessions()
+	for k, v := range dbSessions {
+		if time.Now().Sub(v.lastActivity) > (time.Second * 30) || v.un == "" {
+			delete(dbSessions, k)
+		}
+	}
+	dbSessionsCleaned = time.Now()
+	fmt.Println("AFTER CLEAN")
+	showSessions()
+}
+
+func showSessions() {
+	fmt.Println("*****")
+	for k, v := range dbSessions {
+		fmt.Println(k, v.un)
+	}
+	fmt.Println("")
 }
