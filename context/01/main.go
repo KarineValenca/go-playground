f613d8baf74659bbed9c04583a514074622fd893
Karine Valenca <valenca.karine@gmail.com>
2020-05-09 16:37:34 -0300

    base 64 and context

---
+++ context/01/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	log.Println(ctx)
+	fmt.Fprintln(w, ctx)
+}
