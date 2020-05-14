a4ad1537d6ae8b1c2a460309d76ce10c6b99d4eb
Karine Valenca <valenca.karine@gmail.com>
2020-05-13 21:23:38 -0300

    finished simple crud

--- mongodb/01/controllers/user.go
+++ mongodb/01/controllers/user.go
@@ -6,22 +6,38 @@ import (
 	"../models"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
+type UserController struct{
+	session *mgo.Session
+}
 
-
-type UserController struct{}
-
-func NewUserController() *UserController {
-	return &UserController{}
+func NewUserController(s *mgo.Session) *UserController {
+	return &UserController{s}
 }
 
 func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	u := models.User{
-		Name: "James Bond",
-		Gender: "male",
-		Age: 32,
-		Id: p.ByName("id"),
+	//grab id
+	id := p.ByName("id")
+
+	// verify id is ObjectId hex representation, otherwise return status not found
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	//ObjectIdHex return an ObjectId from the provided hex representation
+	oid := bson.ObjectIdHex(id)
+
+	// composite literal
+	u := models.User{}
+
+	// fetch user
+	if err := uc.session.DB("go-web-dev-db").C("users").FindId(oid).One(&u); err != nil {
+		w.WriteHeader(404)
+		return
 	}
 
 	//marshal into json
@@ -43,11 +59,17 @@ func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ ht
 	// encode/decode for sending;receiving json to/from a stream
 	json.NewDecoder(r.Body).Decode(&u)
 
-	// change id
-	u.Id = "007"
+	// create bson ID
+	u.Id = bson.NewObjectId()
+
+	//store the user in mongodb
+	uc.session.DB("go-web-dev-db").C("users").Insert(u)
 
 	// marshal/unmarshal for having json assigned to a variable
-	uj, _ := json.Marshal(u)
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	//Write content-type, status code, payload
 	w.Header().Set("Content-Type", "application/json")
@@ -56,7 +78,20 @@ func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ ht
 }
 
 func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	//TODO: write code to delete user
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	// Delete user
+	if err := uc.session.DB("go-web-dev-db").C("users").RemoveId(oid); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Write code to delete user\n")
+	fmt.Fprint(w, "Deleted user", oid, "\n")
 }
--- mongodb/01/main.go
+++ mongodb/01/main.go
@@ -4,11 +4,12 @@ import (
 	"./controllers"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"gopkg.in/mgo.v2"
 )
 
 func main()  {
 	r := httprouter.New()
-	uc := controllers.NewUserController()
+	uc := controllers.NewUserController(getSession())
 	
 	r.GET("/user/:id", uc.GetUser)
 	// added route
@@ -16,4 +17,13 @@ func main()  {
 	// added rout plus parameter
 	r.DELETE("/user/:id", uc.DeleteUser)
 	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost")
+
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
